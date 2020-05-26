7c5b0d5ec682ee48d45329bd6611f9f48b5aec38
L04DB4L4NC3R <angadsharma1016@gmail.com>
2020-05-26 02:04:18 +0000

    routers

--- node/apidev/app.js
+++ node/apidev/app.js
@@ -4,14 +4,7 @@ const bp = require("body-parser")
 const app = express()
 app.use(bp.json())
 
-app.get("/", (req, res, next) => {
-				return res.status(201).json({message: "Working"})
-})
-
-app.post("/", (req, res, next) => {
-				return res.json(req.body)
-})
-
+app.use("/", require("./routes/test"))
 app.listen(3000, () => {
 				console.log("Listening....")
 })
