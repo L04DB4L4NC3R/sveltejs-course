b494f3c2aec495cdf6cd23ffe732b976754624b6
L04DB4L4NC3R <angadsharma1016@gmail.com>
2020-05-27 14:27:35 +0000

    fixed indentation

--- node/apidev/app.js
+++ node/apidev/app.js
@@ -2,27 +2,26 @@ const express = require("express")
 const bp = require("body-parser")
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
+const app = express()
 
 dotenv.config()
-
-const app = express()
 app.use(bp.json())
 
 mongoose.connect(process.env.MONGODB_URI, {
-				useNewUrlParser: true,
-				useUnifiedTopology: true
+	useNewUrlParser: true,
+	useUnifiedTopology: true
 }) 
-				.then(conn => {
-								console.log("Connected to MongoDB")
-				})
-				.catch(err => {
-								console.error(err)
-				})
+.then(conn => {
+	console.log("Connected to MongoDB")
+})
+.catch(err => {
+	console.error(err)
+})
 
 app.use("/", require("./routes/test"))
 // crud routes
 app.use("/api/v1", require("./routes/crud"))
 
 app.listen(3000, () => {
-				console.log("Listening....")
+	console.log("Listening....")
 })
