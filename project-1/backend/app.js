31e9169ab3b1cd885b7144a331ae64c50944bc2c
L04DB4L4NC3R <angadsharma1016@gmail.com>
2020-05-31 13:11:51 +0000

    useCreateIndex added in mongodb config

--- project-1/backend/app.js
+++ project-1/backend/app.js
@@ -23,7 +23,8 @@ app.use((err, req, res, next) => {
 
 mongoose.connect(process.env.MONGODB_URI, {
 	useNewUrlParser: true,
-	useUnifiedTopology: true
+	useUnifiedTopology: true,
+	useCreateIndex: true
 }).then(() => {
 	console.log("Connected to the DB");
 }).catch(err => {
